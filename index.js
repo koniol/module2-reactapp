29d2c96bba851ec7971ef036737d218c6a7ab177
Paweł Konik <konius85@gmail.com>
2017-11-14 20:14:57 +0100

    Add class to elemt

--- index.js
+++ index.js
@@ -5,32 +5,40 @@ var favFilms = [
         title : "Karmazynowy przypływ",
         price : '25.99',
         img : './img/krp.jpeg',
+        dscrpt : "Sed id consectetur ligula. Donec pellentesque, purus non molestie rhoncus, mi libero lacinia nibh, non dignissim lectus neque quis lorem."    
     },
     {
         id : 2,
         title : "Cube",
         price : '15.99',
-        img : './img/cube.jpeg'
+        img : './img/cube.jpeg',
+        dscrpt : "Sed id consectetur ligula. Donec pellentesque, purus non molestie rhoncus, mi libero lacinia nibh, non dignissim lectus neque quis lorem."    
+
     },
     {
         id : 3,
         title : "Resident evil",
         price : '15.99',
-        img : './img/resident.jpeg'
+        img : './img/resident.jpeg',
+        dscrpt : "Sed id consectetur ligula. Donec pellentesque, purus non molestie rhoncus, mi libero lacinia nibh, non dignissim lectus neque quis lorem."    
+
     },
     {
         id : 4,
         title : "Władca pirścieni osiem wierz",
         price : '225.99',
-        img : './img/wlad.jpeg'
+        img : './img/wlad.jpeg',
+        dscrpt : "Sed id consectetur ligula. Donec pellentesque, purus non molestie rhoncus, mi libero lacinia nibh, non dignissim lectus neque quis lorem."    
+
     }
 ];
 
 var films = favFilms.map(movie =>{
     return React.createElement('div', {key: movie.id, className: 'box noDecoration'}, 
             React.createElement('h1', {className: 'title'}, movie.title),
-            React.createElement('p', {}, 'Cena ' + movie.price),
-            React.createElement('img', {src: movie.img})
+            React.createElement('p', {}, 'Cena ' + movie.price + ' PLN'),
+            React.createElement('img', {src: movie.img}),
+            React.createElement('p', {}, movie.dscrpt)
         );
 });
 
