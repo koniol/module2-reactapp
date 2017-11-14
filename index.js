f75c28d034a7d5ba62ae5519280ed6db33654ee4
Paweł Konik <konius85@gmail.com>
2017-11-14 19:57:49 +0100

    Add class to elemt

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var favFilms = [
 ];
 
 var films = favFilms.map(movie =>{
-    return React.createElement('li', {key: movie.id, className: 'noDecoration'}, 
+    return React.createElement('div', {key: movie.id, className: 'box noDecoration'}, 
             React.createElement('h1', {className: 'title'}, movie.title),
             React.createElement('p', {}, 'Cena ' + movie.price),
             React.createElement('img', {src: movie.img})
